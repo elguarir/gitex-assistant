0580bddbe558e5344d8eb1308e25ff24cc664456
Mohamed Elguarir <mohaelguarir@gmail.com>
2025-04-10 21:20:56 +0100

    fix: renaming env vars in order to work on aws amplify

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -13,12 +13,12 @@ function generateUUID(): string {
 
 // Configure S3 client
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION!,
+  region: process.env.TEBI_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.TEBI_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.TEBI_SECRET_ACCESS_KEY!,
   },
-  endpoint: process.env.AWS_S3_ENDPOINT!,
+  endpoint: process.env.TEBI_S3_ENDPOINT!,
 });
 
 // Define response type
