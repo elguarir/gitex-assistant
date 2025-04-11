07ac2ad0db99ccb4778af2a92ae7954c45d8234d
Mohamed Elguarir <mohaelguarir@gmail.com>
2025-04-11 19:19:57 +0100

    chore: update font configuration

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,8 +1,9 @@
-import { Fira_Code as FontMono, Inter as FontSans } from 'next/font/google';
+import { Fira_Code as FontMono, Geist as FontSans } from 'next/font/google';
 
 export const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
+  fallback: ['system-ui', 'sans-serif'],
 });
 
 export const fontMono = FontMono({
